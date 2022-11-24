620bdb44aba535b7353d983a24dd74d31c63252e
kayc0des <k.boafo@alustudent.com>
2022-11-24 19:48:16 +0200

    Daily message sample function declaration added

--- functions.py
+++ functions.py
@@ -71,3 +71,26 @@ def reg_success(user_email, username):
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
         smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+def daily_message(email):
+    
+    email_sender = 'icleanapp.py@gmail.com'
+    email_password = 'oovufvagresdoobg'
+    email_receiver = email
+
+    subject = 'Demo'
+    body = f"""Sample Email sent to all users"""
+
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
